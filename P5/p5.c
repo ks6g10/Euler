1b39da01d2910be24abcfbef9c57748f34f68573
Kim Svensson <ks6g10@soton.ac.uk>
2012-04-06 17:01:50 +0100

    Small debug of one var, added license and disclamer of problem

--- P5/p5.c
+++ P5/p5.c
@@ -1,18 +1,41 @@
+/*Euler problem 5
+  Copyright (C) 2012 Kim Svensson ks@linux.com
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
-#define NUM_THREADS     1
+#define NUM_THREADS     5
 typedef uint32_t myint;
 
 static const myint max = 20;
 static myint min = UINT32_MAX;
-static multi;
+static myint multi;
 static myint acount;
 static myint * usearray;
-//20 40 60 80 100 120
 myint test(myint count);
+
+/*Took a wrong approach on this one, 
+  should have calculated prime factors of it,
+  and then multiplied the highes prime factors
+*/
+
+
 void *run(void * count)
 {
 	register myint icount =  *((myint *) count);
